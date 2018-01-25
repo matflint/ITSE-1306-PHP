c31bb4c31654615c5e31ce2b7a1ff56003943cc8
matflint <35752762+matflint@users.noreply.github.com>
2018-01-25 04:34:15 -0600

    Add files via upload

---
+++ CH3_Matthew.Flint_Hello.World.php
@@ -0,0 +1,21 @@
+<?php
+<html>
+	<head>
+		<title>CH3 Assignment</title>
+	</head>
+	<body>
+		echo "Hello World!";
+		// Single Line Comment
+		/* Matthew Flint, ITSE-1306, 1-25-18, CH3 code*/
+		$StudentName = "Matthew";
+		echo $StudentName;
+		$array = array('o',' ',' '),
+					  (' ','o',' '),
+					  (' ',' ','o');
+		echo $array[1][1];
+		$num = 12345;
+		echo substr($num, 3,1);
+		echo 3 + 1;
+		echo "a time stamp"
+	</body>
+</html>
